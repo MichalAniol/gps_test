e256ea646a485d76cc6d5ebb9fc0bd7771eba0f1
Michał Anioł <michal.janusz.aniol@gmail.com>
2020-03-08 12:52:50 +0100

    1

---
+++ index.js
@@ -0,0 +1,26 @@
+const gps = document.getElementById('gps');
+const counter = document.getElementById('counter');
+
+const getLocation = () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        gps.innerHTML = "Geo Location not supported by browser";
+    }
+}
+
+const showPosition = (position) => {
+    let location = {
+        longitude: position.coords.longitude,
+        latitude: position.coords.latitude
+    }
+    gps.innerHTML = location.latitude + '<br>' + location.longitude
+}
+
+var timeCounter = 0;
+
+setInterval(() => {
+    getLocation();
+    timeCounter++;
+    counter.innerHTML = timeCounter;
+}, 300);
