4381b16497e2b97761facec6fb96ac00789e5be3
Michał Anioł <michal.janusz.aniol@gmail.com>
2020-03-08 21:24:51 +0100

    8

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ const waitForPromision = setInterval(() => {
         checkingGeo = setInterval(() => {
             getLocation();
             timeCounter++;
-            counter.innerHTML = '+_' + timeCounter + '_';
+            counter.innerHTML = '+_' + timeCounter + '_+';
         }, 300);
     }
 }, 300
